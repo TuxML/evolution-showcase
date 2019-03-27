6d731a5bbab0f8f82e341a6d2479e3aaa30ccfd7
Mathieu Acher <mathieu.acher@irisa.fr>
2019-03-27 15:45:12 +0100

    First commit

---
+++ evokernelversions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import subprocess
+
+
+if __name__ == "__main__":
+   linux_versions = [13.1, 14.1, 16.1]
+   for linux_version in linux_versions:
+        cmd = "python3 kernel_generator.py --tiny --linux4_version {} --dev 1".format(str(linux_version))
+        print(cmd)
+        try:
+            result = subprocess.check_output(args=cmd,
+                shell=True,
+                universal_newlines=True
+            )
+            print(result)
+            print("END")
+        except subprocess.CalledProcessError as ex:
+            raise NotImplementedError("unable to compile") from ex
+        if len(result) == 0:
+            raise NotImplementedError("Unable to process compilation status")
+        
